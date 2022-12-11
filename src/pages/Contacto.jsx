ffb34fa6af5e79d431c8d566bb449ca6fe9cf455
bruno <brunodecruz.05@gmail.com>
2022-12-11 01:42:47 -0300

    estilos mobile hechos

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
-
+import '../styles/contacto.css'
 export const Contacto = () => {
   return (
-    <div>Contacto</div>
+    <div>     
+        <div className='Contacto-Img'>
+          <h2>“La energía y la persistencia conquistan todo”</h2>
+        </div>
+        <div className='Contacto-Container'>
+          <h2>contacto</h2>
+          <form action="" className='form-container'>
+            <div>
+            <label htmlFor="NOMBRE">NOMBRE:</label>
+            <input type="text" name="NOMBRE" id="" placeholder='Escribe tu nombre...' />
+            </div>
+            <div>
+            <label htmlFor="CORREO ELECTRONIC">CORREO ELECTRONICO:</label>
+            <input type="email" name="CORREO ELECTRONICO" id="" placeholder='Escribe tu correo electrónico...' />
+            </div>
+            <div>
+            <label htmlFor="MENSAJE">MENSAJE:</label>
+            <textarea name="MENSAJE" id="" rows="10"placeholder='Déjanos un mensaje...'></textarea>
+            </div>
+            <button type="submit">ENVIAR</button>
+          </form>
+        </div>
+    </div>
   )
 }
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { NavBar } from '../components/NavBar'
+import { Contacto } from './Contacto'
+import { Footer } from './Footer'
+import { Header } from './Header'
 import { Metodologias } from './Metodologias'
 import { Programa } from './Programa'
 import { Proyecto } from './Proyecto'
@@ -8,9 +11,12 @@ import { Proyecto } from './Proyecto'
 export const Inicio = () => {
   return (
     <>
+      <Header/>
       <Metodologias/>
       <Proyecto/>
       <Programa/>
+      <Contacto/>
+      <Footer/>
     </>
   )
 }
